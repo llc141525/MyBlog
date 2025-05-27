a3dc546c56d780ddf61cf30f279b2576444a60ae
Lei Lecen <145575708+llc141525@users.noreply.github.com>
2025-05-27 09:01:18 +0800

    机械革命 llc (#11)
    * 用户完成, 在写文章
    
    * web更新
    
    * 添加首页获取评论个数的功能

--- web/src/components/ArticleCard.vue
+++ web/src/components/ArticleCard.vue
@@ -10,11 +10,13 @@
     <br> users<br>
     {{ user }}
   </v-card>
+  <v-btn @click="getArticleHome"> 获取文章首页 </v-btn>
 
 </template>
 
 <script lang="ts" setup>
   import type { ArticleDetailResponse, ArticleHomeResponse, CommentResponse, UsersResponse } from '@/types';
+  import {articleApi} from "@/api/article.ts";
   // 测试所有接口
 
   const articleHome = ref<ArticleHomeResponse>()
@@ -22,20 +24,22 @@
   const comment = ref<CommentResponse[]>()
   const user = ref<UsersResponse>()
 
-  onMounted(async () => {
-  // articleHome.value = await articleApi.getArticleHome(1)
-  // articleDetail.value = await articleApi.getArticleDetail(4)
-  // comment.value = await commentApi.getComment(4)
-  // user.value = await usersApi.login({ username: 'abc', password: '123' })
-  // await usersApi.logout();
-  // usersApi.register({ username: 'tttttffffft', password: 'sdfsdfs' })
-  // usersApi.update({ username: 'testtestttt' })
-  // commentApi.createComment({ content: '测试测试', articleId: 4 })
-  // commentApi.deleteComment(19)
-  // articleApi.createArticle({ title: 'title', content: 'content', cover_url: 'test', status: false })
-  // articleApi.deleteArticle(6)
-  // articleApi.updateArticle({ articleId: 9, content: '测试' })
-  })
-
+  // onMounted(async () => {
+  // // articleHome.value = await articleApi.getArticleHome(1)
+  // // articleDetail.value = await articleApi.getArticleDetail(4)
+  // // comment.value = await commentApi.getComment(4)
+  // // user.value = await usersApi.login({ username: 'abc', password: '123' })
+  // // await usersApi.logout();
+  // // usersApi.register({ username: 'tttttffffft', password: 'sdfsdfs' })
+  // // usersApi.update({ username: 'testtestttt' })
+  // // commentApi.createComment({ content: '测试测试', articleId: 4 })
+  // // commentApi.deleteComment(19)
+  // // articleApi.createArticle({ title: 'title', content: 'content', cover_url: 'test', status: false })
+  // // articleApi.deleteArticle(6)
+  // // articleApi.updateArticle({ articleId: 9, content: '测试' })
+  // })
+  const getArticleHome = async ()=>{
+    articleHome.value = await articleApi.getArticleHome(1)
+  }
 
 </script>
--- web/src/pages/[page].vue
+++ web/src/pages/[page].vue
@@ -1,13 +1,15 @@
 <template>
   <v-sheet> 主页 {{ page }}</v-sheet>
-  <article-card />
+  <test/>
 </template>
 
 <script lang="ts" setup>
+  import Test from "@/components/Test.vue";
+
   const route= useRoute()
 
   // 获取路由参数, page 是页数.
   const page= (route.params as { page: string }).page
-  
+
 
 </script>
